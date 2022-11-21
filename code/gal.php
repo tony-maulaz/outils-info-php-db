95cb537815f80faabc1cf0bdef7f6db17c858e8e
Maulaz Tony <tony.maulaz@heig-vd.ch>
2022-11-21 09:21:28 +0100

    Add font-awesome

--- code/gal.php
+++ code/gal.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link href="style.css" rel="stylesheet" media="all">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <title>First PHP</title>
  </head>
  <body>
