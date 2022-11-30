8546c518c60cda19004b7e320c903a3f8629547d
Maulaz Tony <tony.maulaz@heig-vd.ch>
2022-11-30 13:39:36 +0100

    Add js exemple

--- code/test.js
+++ code/test.js
@@ -2,3 +2,14 @@ console.log("Hello")
 
 let a = 12
 console.log("Var a " + a)
+
+const element = document.getElementById("img1")
+
+alert("La source de l'image va changer")
+
+if( false )
+    element.src = "images/test.jpg"    
+else
+    element.src = ""    
+
+element.style.width = "300px"
